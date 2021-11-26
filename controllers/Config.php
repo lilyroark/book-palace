c957cc7b7f1c870ba740a63631bccdb709d85bae
lilyroark <lar6jk@virginia.edu>
2021-11-26 12:35:12 -0500

    Changed config file for local development

--- controllers/Config.php
+++ controllers/Config.php
@@ -5,7 +5,8 @@ class Config {
     private $base_url;
 
     public function __construct() {
-        $this->base_url = "cs4750/book-palace";
+        $this->base_url = "";
+        //Changed base url for local development "cs4750/book-palace";
     }
 
     public function getURL() {
